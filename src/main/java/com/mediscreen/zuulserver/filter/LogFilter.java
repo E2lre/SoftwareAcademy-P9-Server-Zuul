e83f9af212107446453fd31b7e66af6a8da7b5ab
Eric Delaire <S058601@siege.axa-fr.intraxa>
2020-12-11 19:19:31 +0100

    Ajustement du pom.xml pour avoir la bonne version de sprintboot

--- src/main/java/com/mediscreen/zuulserver/filter/LogFilter.java
+++ src/main/java/com/mediscreen/zuulserver/filter/LogFilter.java
@@ -3,15 +3,16 @@ package com.mediscreen.zuulserver.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class LogFilter extends ZuulFilter {
-    Logger log = LoggerFactory.getLogger(this.getClass());
+    //Logger log = LoggerFactory.getLogger(this.getClass());
+    private static final Logger logger = LogManager.getLogger(LogFilter.class);
     @Override
     public String filterType() {
         return "pre";
@@ -30,8 +31,9 @@ public class LogFilter extends ZuulFilter {
     @Override
     public Object run() throws ZuulException {
         HttpServletRequest req = RequestContext.getCurrentContext().getRequest();
-        log.warn("*** Requete vue par Zuul :" + req.getRequestURL());
-        log.warn("*** ----- Avec :"+ req.getAuthType());
+        logger.info("*** Requete vue par Zuul :" + req.getRequestURL());
+        logger.info("*** ----- Avec getAuthType :"+ req.getAuthType());
+        logger.info("*** ----- Avec methode :"+ req.getMethod());
         return null;
     }
 }
